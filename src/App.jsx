130864babe6109c4d32b0d6476b9089da7cf58c0
amo-tech <kaomi.x1003@gmail.com>
2022-12-31 13:21:23 +0900

    page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ export const App = () => {
     return(
         <>
           <h1>credit</h1>
+          <button>toggleにする</button>
+          <input></input>
+
+          <h2>history</h2>
+          <button>しめかわいに切り替える</button>
     
         </>
     )
