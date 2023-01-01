bd93e2ee3c714f81237452cbe55c24d5495d09f3
amo-tech <kaomi.x1003@gmail.com>
2023-01-01 11:35:58 +0900

    radio button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 export const App = () => {
 
+  const handleChange = () => {
+    return(
+        "hoge"
+    );
+  };
+
     return(
         <>
           <h1>credit</h1>
-          <button>toggleにする</button>
-          <input></input>
+          <label>
+          <input 
+            type="radio" 
+            value="shime" 
+            onChange={handleChange} 
+            checked={"a"}></input>
+            shime or kaomi 66miteyaru
+          </label>
+          
+
 
           <h2>history</h2>
           <button>しめかわいに切り替える</button>
