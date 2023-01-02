fdabcf3e85936371cd47daed195cb2b5b4f762e3
amo-tech <kaomi.x1003@gmail.com>
2023-01-02 21:45:33 +0900

    radio button

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
+import { useState } from "react";
+
 export const App = () => {
+  const radioCollection = ["shime", "kaomi"];
+  const [borrowed, setBorrowed] = useState("kaomi");
+  const onChange = (e) => setBorrowed(e.target.value);
 
-  const handleChange = () => {
-    return(
-        "hoge"
-    );
-  };
+  return (
+    <>
+      <h1>credit</h1>
 
-    return(
-        <>
-          <h1>credit</h1>
-          <label>
-          <input 
-            type="radio" 
-            value="shime" 
-            onChange={handleChange} 
-            checked={"a"}></input>
-            shime or kaomi 66miteyaru
+      {radioCollection.map((value) => {
+        return (
+          <label key={value}>
+            <input
+              type="radio"
+              value={value}
+              checked={borrowed === value}
+              onChange={onChange}
+            >
+            </input>
+            {value}
           </label>
-          
+        )
+      })}
+      <br/>
+      <input type="textarea"></input>
+      {/* ここに書いたものをconsolelognに出力させる */}
 
 
-          <h2>history</h2>
-          <button>しめかわいに切り替える</button>
-    
-        </>
-    )
+
+
+      <h2>history</h2>
+      <button>しめかわいに切り替える</button>
+
+    </>
+  )
 };
